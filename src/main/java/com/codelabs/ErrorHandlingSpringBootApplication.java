58049b46234d1216a45744f6acebbfdeb93a15fb
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2024-06-28 09:39:52 +0530

    Initial commit

---
+++ src/main/java/com/codelabs/ErrorHandlingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.codelabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorHandlingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorHandlingSpringBootApplication.class, args);
+	}
+
+}
