18082577f9f942f7267155350f773c5b5a4d33d2
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2024-06-29 09:58:47 +0530

    adding dependency and DTos

--- src/main/java/com/codelabs/dto/ApiError.java
+++ src/main/java/com/codelabs/dto/ApiError.java
@@ -1,4 +1,94 @@
 package com.codelabs.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.CollectionUtils;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ApiError {
+    private HttpStatus status;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private LocalDateTime timestamp;
+    private String message;
+    private String debugMessage;
+    private List<ApiSubError> subErrors;
+
+    public ApiError() {
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public ApiError(HttpStatus status) {
+        this();
+        this.status = status;
+    }
+
+    public ApiError(HttpStatus status, Throwable ex) {
+        this(status);
+        this.message = "Expected Error";
+        this.debugMessage = ex.getLocalizedMessage();
+    }
+
+    public  ApiError(HttpStatus status, String message, Throwable ex) {
+        this(status);
+        this.message = message;
+        this.debugMessage = ex.getLocalizedMessage();
+    }
+
+    public void addSubError(ApiSubError subError) {
+        if(CollectionUtils.isEmpty(subErrors)) {
+            subErrors = new ArrayList<>();
+        }
+        subErrors.add(subError);
+    }
+
+    public void addValidationError(String object, String field, Object rejectedValue, String message) {
+        addSubError(new ApiValidationError(object, field, rejectedValue, message));
+    }
+
+    public void addValidationError(String object, String message) {
+        addSubError(new ApiValidationError(object, message));
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDebugMessage() {
+        return debugMessage;
+    }
+
+    public void setDebugMessage(String debugMessage) {
+        this.debugMessage = debugMessage;
+    }
+
+    public List<ApiSubError> getSubErrors() {
+        return subErrors;
+    }
+
+    public void setSubErrors(List<ApiSubError> subErrors) {
+        this.subErrors = subErrors;
+    }
 }
