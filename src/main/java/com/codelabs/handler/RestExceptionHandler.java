c94a407f9064820f94a1b46fdde9faacae4e0f87
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2024-06-30 11:56:51 +0530

    Update RestExceptionHandler.java

--- src/main/java/com/codelabs/handler/RestExceptionHandler.java
+++ src/main/java/com/codelabs/handler/RestExceptionHandler.java
@@ -1,8 +1,24 @@
 package com.codelabs.handler;
 
+import com.codelabs.dto.ApiError;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(
+            MissingServletRequestParameterException ex, HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+        String error = ex.getParameterName() + " parameter is missing";
+        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, error, ex));
+    }
 
+    private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
 }
